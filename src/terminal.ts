60722bdde3d98eb0996ca2fa99dff422ce7cb860
Howcanoe WANG <howcanoewang@gmail.com>
2023-08-23 11:45:47 +0900

    support input event

--- src/terminal.ts
+++ src/terminal.ts
@@ -1,4 +1,4 @@
-import {Terminal} from "./xterm.js/xterm.js"
+import { Terminal } from 'xterm';
 
 export function loadXterm() {
     // 添加<link>
@@ -8,9 +8,9 @@ export function loadXterm() {
     document.head.appendChild(link);
 
     // 添加<script>
-    // const script = document.createElement('script');
-    // script.src = "./plugins/siyuan-plugin-semantic-search/xterm.js/xterm.js";
-    // document.head.appendChild(script);
+    const script = document.createElement('script');
+    script.src = "./plugins/siyuan-plugin-semantic-search/xterm.js/xterm.js";
+    document.head.appendChild(script);
 }
 
 export function initXterm() {
@@ -18,6 +18,28 @@ export function initXterm() {
         cursorBlink: true,
     });
     term.open(document.getElementById('terminal'));
-    term.writeln('SiYuan XTerm [Ver 0.0.1]')
-    term.write('SY \x1B[1;3;31m~\x1B[0m $ ')
+    term.writeln('SiYuan XTerm [Ver 0.0.1]');
+    term.write('SY \x1B[1;3;31m~\x1B[0m $ ');
+
+    term.onKey(e =>{
+        /*
+            esc = 27
+            回车 = 13
+            上下左右 = 37,38,39,40
+        */
+        var cmd = ''
+        let code = e.domEvent.code;
+        // console.log(code, typeof code)
+        if (code === 'Enter'){
+            term.write(e.key + '\n')
+            console.log('cmd:',cmd)
+            cmd = ''
+        }else if (code === 'Backspace') {
+            term.write('\b \b')
+            cmd = cmd.substring(0,cmd.length-1)
+        } else{
+            cmd = cmd + e.key
+            term.write(e.key)
+        }
+    }) 
 }
